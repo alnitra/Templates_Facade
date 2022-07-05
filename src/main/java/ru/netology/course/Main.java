29979158041a8aa99ab8672d533af60744dd054a
alnitra <alnit@yandex.ru>
2022-07-05 20:24:20 +0300

    first commit

---
+++ src/main/java/ru/netology/course/Main.java
@@ -0,0 +1,14 @@
+package ru.netology.course;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args ) {
+        BinOps binOpsExmpl1 = new BinOps();
+        System.out.println(binOpsExmpl1.sum("1001","1011"));
+        System.out.println(binOpsExmpl1.mult("1001","1011"));
+    }
+}
